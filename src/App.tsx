85a372df83ade0ecf098edb0f362b3ae84c36369
sahel.hamed19@gmail.com <sahel1372>
2020-10-29 10:53:23 +0330

    fix: eslint errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React, { ReactElement } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const App = (): ReactElement => {
+const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -21,6 +21,6 @@ const App = (): ReactElement => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
